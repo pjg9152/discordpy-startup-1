ee1e6e306d0d789d5ec7fd285e2f332b0cccc0d3
pjg9152 <54145235+pjg9152@users.noreply.github.com>
2019-08-15 23:46:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,30 +1,19 @@
-﻿# -*- coding: utf-8 -*-
-import discord
-import asyncio
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-# ログを削除する処理
-@client.event
-async def on_message(message):
-    await asyncio.sleep(86400)
-    count = 0
-    while count < 5:
-        try:
-            await client.delete_message(message)
-            print("%sの発言を削除しました"%message.author.name)
-            break
-        except:
-            count += 1
-            print("発言の削除失敗%d回目"%count)
-            await asyncio.sleep(30)
-@client.event
-async def on_message(message):
-    if message.content == '!clean':
-        if message.author.guild_permissions.administrator:
-            await message.channel.purge()
-            await message.channel.send('スイープしたじょ🎵')
-        else:
-            await message.channel.send('何様のつもり？')
 
-client.run("NDU2NDUxNDI0ODU5OTc5Nzc4.DgYFqA.5Mc8q9y9nWU8kSvvBAhp6BNGfD4")
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
